5f4793ba10e0fcc44e720bee03ed5eddcfd0159b
adaptable-device-5942 <vh2727@yahoo.com>
2018-10-16 19:54:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -32,7 +32,7 @@ def now_serving(katz_deli)
   if katz_deli == []
     puts("There is nobody waiting to be served!")
   else
-    puts("Currently serving " + katz_deli[0] + ".")
+    puts("Currently serving " + katz_deli[0].to_s + ".")
     katz_deli.shift
   end
 end
